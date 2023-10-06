c4f2455f661988a65fe3c52d35eef28956bb2781
Facundo Carballo <carballofacundo70@gmail.com>
2023-10-06 15:27:37 -0300

    Business not found page

--- pages/business/[id].tsx
+++ pages/business/[id].tsx
@@ -6,25 +6,46 @@ import { useHomeProvider } from "@/src/contexts/home";
 import { useRouter } from "next/router";
 import { Box } from "@chakra-ui/react";
 import { BusinessSettings } from "@/src/subpages/business/BusinessSettings";
+import { useBusinessProvider } from "@/src/contexts/business";
+import { Business } from "@/src/types/business";
+import BusinessNotFound from "@/src/subpages/business/BusinessNotFound";
 
 export default function BusinessProfilePage() {
   // Attributes
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const [theBusiness, setTheBusiness] = React.useState<Business | undefined>(
+    undefined
+  );
   const router = useRouter();
   const url = router.asPath.split("/");
+  const theBusinessId = url[url.length - 1];
   // Context
   const { user } = useHomeProvider();
+  const { business } = useBusinessProvider();
   // Methods
-
+  const handleGetTheBusiness = async () => {
+    const finded = business?.find((b) => b.id === theBusinessId);
+    if (finded === undefined) {
+      // Busco el business por mi cuenta con una consulta.
+      setLoading(false);
+      return;
+    }
+    setTheBusiness(finded);
+    setLoading(false);
+  };
   React.useEffect(() => {
     if (user === undefined) {
       router.push("/");
     }
+    handleGetTheBusiness();
   }, []);
   // Component
+  if (loading) return null;
+  if (theBusiness === undefined) return <BusinessNotFound />;
   return (
     <>
       <Head>
-        <title>Business Profile</title>
+        <title>{theBusiness.name} - Profile</title>
         <meta
           name="description"
           content="App to organize your business and get new clients."
---
+++ src/subpages/business/BusinessNotFound.tsx
@@ -0,0 +1,17 @@
+import { VStack, Text, Box } from "@chakra-ui/react";
+import React from "react";
+
+export default function BusinessNotFound() {
+  // Attributes
+  // Context
+  // Methods
+  // Component
+  return (
+    <VStack w="full">
+      <Box h="200px" />
+      <Text variant="description">
+        {"There is not business in Booking App with this id :("}
+      </Text>
+    </VStack>
+  );
+}
