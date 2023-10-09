15c87367f929dacc10b9e8dd5d75f732f03e7db6
Facundo Carballo <carballofacundo70@gmail.com>
2023-10-09 14:51:38 -0300

    Show amount of books

--- src/subpages/branch/BranchBooks.tsx
+++ src/subpages/branch/BranchBooks.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { VStack, HStack, Spacer, Box } from "@chakra-ui/react";
+import { VStack, HStack, Spacer, Box, Text } from "@chakra-ui/react";
 import { SwitchAvailableBusy } from "@/src/components/branch/SwitchAvailableBusy";
 import { SelectDay } from "@/src/components/branch/SelectDay";
 import { MyBooks } from "../books/MyBooks";
+import { useBookProvider } from "@/src/contexts/book";
 
 export const BranchBooks = () => {
   // Attributes
   // Context
+  const { books } = useBookProvider();
   // Methods
   // Component
   return (
@@ -16,10 +18,14 @@ export const BranchBooks = () => {
           <Box w="10px" />
           <SwitchAvailableBusy />
           <Spacer />
+          <Text variant="caption">
+            {books === undefined ? 0 : books.length} Books
+          </Text>
+          <Spacer />
           <SelectDay />
           <Box w="10px" />
         </HStack>
-        <Box h='10px' />
+        <Box h="10px" />
         <MyBooks />
       </VStack>
     </>
