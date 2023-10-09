62e9b3e0fdf942f93a1d7d9acd3e057a3c82441b
Facundo Carballo <carballofacundo70@gmail.com>
2023-10-09 14:49:00 -0300

    Grid display

--- src/subpages/books/MyBooks.tsx
+++ src/subpages/books/MyBooks.tsx
@@ -14,6 +14,14 @@ import { BookUnavailable } from "@/src/components/book/BookUnavailable";
 
 export const MyBooks = () => {
   // Attributes
+  const templeteColumns = {
+    "2xl": "repeat(10, 1fr)",
+    xl: "repeat(9, 1fr)",
+    lg: "repeat(7, 1fr)",
+    md: "repeat(5, 1fr)",
+    sm: "repeat(3, 1fr)",
+    base: "repeat(2, 1fr)",
+  };
   // Context
   const { booksAvailables, showAvailable, books } = useBookProvider();
   // Methods
@@ -27,15 +35,19 @@ export const MyBooks = () => {
           <Spacer />
         </HStack>
         <Box h="10px" />
-        <Grid w="full" templateColumns="repeat(6, 1fr)" gap={6}>
-          {showAvailable
-            ? booksAvailables.map((b, idx) => (
-                <BookAvailable time={b} key={idx} />
-              ))
-            : books !== undefined
-            ? books.map((b) => <BookUnavailable key={b.id} book={b} />)
-            : null}
-        </Grid>
+        <HStack>
+          <Box w="10px" />
+          <Grid w="full" templateColumns={templeteColumns} gap={6}>
+            {showAvailable
+              ? booksAvailables.map((b, idx) => (
+                  <BookAvailable time={b} key={idx} />
+                ))
+              : books !== undefined
+              ? books.map((b) => <BookUnavailable key={b.id} book={b} />)
+              : null}
+          </Grid>
+          <Box w="10px" />
+        </HStack>
         {showAvailable && booksAvailables.length === 0 ? (
           <Text variant="empty">
             {"Don't have a book availables for this day."}
