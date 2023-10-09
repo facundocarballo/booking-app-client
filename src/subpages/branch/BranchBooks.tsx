fb4f0702c0987d65f18e96f7faabc8b36f00f693
Facundo Carballo <carballofacundo70@gmail.com>
2023-10-09 07:44:24 -0300

    remove unnecesary things

--- src/subpages/branch/BranchBooks.tsx
+++ src/subpages/branch/BranchBooks.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import { VStack, HStack, Spacer, Box } from "@chakra-ui/react";
 import { SwitchAvailableBusy } from "@/src/components/branch/SwitchAvailableBusy";
-import { useBookProvider } from "@/src/contexts/book";
 import { SelectDay } from "@/src/components/branch/SelectDay";
 import { MyBooks } from "../books/MyBooks";
 
 export const BranchBooks = () => {
   // Attributes
   // Context
-  const { setBooks, showAvailable, daySelected } = useBookProvider();
   // Methods
-  const handleGetBooks = async () => {};
-
-  React.useEffect(() => {
-    handleGetBooks();
-  }, [showAvailable, daySelected]);
   // Component
   return (
     <>
